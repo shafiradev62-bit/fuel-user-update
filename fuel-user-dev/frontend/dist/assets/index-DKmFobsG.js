4506f398f4b22a3648e9acb40b153ba918479b40
1wasilah1 <m.wasilahhadi@gmail.com>
2026-02-02 03:20:03 +0700

    cleanup: remove backend and deployment files
    - Remove entire backend folder
    - Remove all deployment scripts and configs
    - Remove database SQL files
    - Remove Docker and K8s configs
    - Keep only frontend React app
    - Clean project structure

---
+++ fuel-user-dev/frontend/dist/assets/index-DKmFobsG.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-C-3XzAdB.js","assets/index-C6zoZ9mA.js","assets/index-DicxzG6e.css"])))=>i.map(i=>d[i]);
+import{r as o,_ as t}from"./index-C6zoZ9mA.js";const _=o("GoogleAuth",{web:()=>t(()=>import("./web-C-3XzAdB.js"),__vite__mapDeps([0,1,2])).then(e=>new e.GoogleAuthWeb)});export{_ as GoogleAuth};
